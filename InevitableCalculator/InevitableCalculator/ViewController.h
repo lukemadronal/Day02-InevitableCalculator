1e20ab579906ceee9c1b86a5f4f14cbc004c2bc9
Luke Madronal <madronal@lafayette.edu>
2015-09-22 12:36:06 -0400

    Initial commit of inevitable calculator.

---
+++ InevitableCalculator/InevitableCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InevitableCalculator
+//
+//  Created by Luke Madronal on 9/22/15.
+//  Copyright © 2015 Luke Madronal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InevitableCalculator/InevitableCalculator/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  InevitableCalculator
+//
+//  Created by Luke Madronal on 9/22/15.
+//  Copyright © 2015 Luke Madronal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (nonatomic,weak) IBOutlet UIButton *oneButton;
+@property (nonatomic,weak) IBOutlet UIButton *clearButton;
+@property (nonatomic,weak) IBOutlet UIButton *equalsButton;
+@property (nonatomic,weak) IBOutlet UIButton *plusButton;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
